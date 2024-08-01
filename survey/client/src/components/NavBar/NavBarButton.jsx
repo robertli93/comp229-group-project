5fcca192793d03f8a3ca9e8f3d316810c4bedae7
ForbiddenRite <itisjustforwork@gmail.com>
2024-08-01 15:16:20 -0400

    stage 3 - putting FE and BE together

---
+++ survey/client/src/components/NavBar/NavBarButton.jsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import { Link, useMatch } from "react-router-dom";
+
+function NavBarButton({ href, pattern, children }) {
+  const isActive = useMatch(pattern ?? (href ?? ""));
+  return (
+    <Link
+      to={href}
+      className={cn("transition-colors hover:text-foreground/80 text-foreground/60", {
+        "text-foreground":isActive
+      })}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default NavBarButton;
